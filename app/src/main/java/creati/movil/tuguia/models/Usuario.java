eed0e830a058f5de160b8f72dfc1d4ccccde0c1f
Dario Chamorro <dario.chamorro.v@gmail.com>
2015-08-12 21:20:18 -0500

    validacion de inicio de sesion con sqlite

---
+++ app/src/main/java/creati/movil/tuguia/models/Usuario.java
@@ -0,0 +1,88 @@
+package creati.movil.tuguia.models;
+
+import android.content.Context;
+
+import com.orm.SugarContext;
+import com.orm.SugarRecord;
+
+import java.util.List;
+
+/**
+ * Created by Dario Chamorro on 12/08/2015.
+ */
+public class Usuario extends SugarRecord{
+    String nombre,usr, pass, urlusr, urlbanner;
+
+    public Usuario() {
+    }
+
+    //region Getter & Setter
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUsr() {
+        return usr;
+    }
+
+    public void setUsr(String usr) {
+        this.usr = usr;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getUrlusr() {
+        return urlusr;
+    }
+
+    public void setUrlusr(String urlusr) {
+        this.urlusr = urlusr;
+    }
+
+    public String getUrlbanner() {
+        return urlbanner;
+    }
+
+    public void setUrlbanner(String urlbanner) {
+        this.urlbanner = urlbanner;
+    }
+    //endregion
+
+    //region Sqlite
+    public static void init(Context context){
+        SugarContext.init(context);
+
+        if(count(Usuario.class)<1) {
+            Usuario usuario = new Usuario();
+            usuario.setNombre("Dario Chamorro");
+            usuario.setUsr("dario");
+            usuario.setPass("123");
+            usuario.setUrlusr("https://goo.gl/zonrYM");
+            usuario.setUrlbanner("http://goo.gl/V7irxd");
+
+            usuario.save();
+        }
+    }
+
+    public static Usuario findUsuarioByUsrAndPass(String usr, String pass){
+        Usuario usuario =null;
+
+        List<Usuario> result = find(Usuario.class, "usr = ? AND pass = ?", usr, pass);
+
+        if(result.size()>0)
+            usuario = result.get(0);
+
+        return usuario;
+    }
+    //endregion
+}
