f5d47087e49a7b3e15e71060a448a793a5766f29
Dario Chamorro <dario.chamorro.v@gmail.com>
2015-08-13 20:02:25 -0500

    Almacenamiento en sharedpreference de la información de usuario

--- app/src/main/java/creati/movil/tuguia/LoginActivity.java
+++ app/src/main/java/creati/movil/tuguia/LoginActivity.java
@@ -1,6 +1,7 @@
 package creati.movil.tuguia;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +12,7 @@ import android.view.View;
 import android.widget.Button;
 
 import creati.movil.tuguia.models.Usuario;
+import creati.movil.tuguia.util.AppUtil;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -18,10 +20,16 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     TextInputLayout usr, pass;
     View table;
 
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        preferences = getSharedPreferences(AppUtil.PREFERENCE_NAME, MODE_PRIVATE);
+        editor = preferences.edit();
 
         register = (Button) findViewById(R.id.btn_register);
         login = (Button) findViewById(R.id.btn_login);
@@ -63,8 +71,16 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             pass.setErrorEnabled(true);
             pass.setError(getString(R.string.login_error));
         }else{
+
+            editor.putBoolean(AppUtil.KEY_LOGIN, true);
+            editor.putString(AppUtil.KEY_USR_NAME, usuario.getNombre());
+            editor.putString(AppUtil.KEY_USR_IMG, usuario.getUrlusr());
+            editor.putString(AppUtil.KEY_USR_IMG_BANNER, usuario.getUrlbanner());
+            editor.commit();
+
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
+            finish();
         }
 
     }
---
+++ app/src/main/java/creati/movil/tuguia/util/AppUtil.java
@@ -0,0 +1,13 @@
+package creati.movil.tuguia.util;
+
+/**
+ * Created by Dario Chamorro on 13/08/2015.
+ */
+public class AppUtil {
+    public static final String PREFERENCE_NAME="preference";
+    public static final String KEY_USR_NAME="username";
+    public static final String KEY_USR_IMG="user_img";
+    public static final String KEY_USR_IMG_BANNER="user_img_banner";
+    public static final String KEY_LOGIN="login";
+
+}
