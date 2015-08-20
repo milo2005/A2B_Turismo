208bb731e14d6f437403f88bae6504a8877819b6
Dario Chamorro <dario.chamorro.v@gmail.com>
2015-08-19 20:22:26 -0500

    Implementacion de CoordinatorLayout y CollapsingToolbar

--- app/src/main/java/creati/movil/tuguia/DetailActivity.java
+++ app/src/main/java/creati/movil/tuguia/DetailActivity.java
@@ -1,16 +1,29 @@
 package creati.movil.tuguia;
 
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class DetailActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+    CollapsingToolbarLayout collapsingToolbarLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbar);
+        collapsingToolbarLayout.setTitle("Restaurante");
+
+
     }
 
     @Override
